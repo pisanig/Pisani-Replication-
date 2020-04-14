000c4387553e0eec098c1b46e18d45d9f0e39a3c
Gabriel Pisani <pisani.gabriel@seznam.cz>
2020-04-15 00:48:10 +0200

    test2 - Loading data, Data Wrangling (Rescaling for Control)

--- replication rename.R
+++ replication rename.R
@@ -6,7 +6,47 @@ library(here)
 
 # Load data ---------------------------------------------------------------
 
-dta <- haven::read_dta(here::here("Replication Data.dta"))
+dat <- haven::read_dta(here::here("Replication Data.dta"))
 
-# Data Wrangling
+# Data Wrangling ----------------------------------------------------------
 
+#Setting variables as numeric in order to be able to treat them as numeric values.
+dat <- dat %>% 
+  mutate(Q20 = as.numeric(Q20)) %>%
+  drop_na(Q20) %>% 
+  mutate(Q43 = as.numeric(Q43)) %>% 
+  drop_na(Q43)
+#Creating groups for different experimental conditions
+
+#Group A - control group
+A_control <- dat %>% 
+  filter(control == 1) %>% # Selecting only observations from the control group
+  mutate(anger = ifelse(Q20 == 1, 0,
+                        ifelse(Q20 == 2, 0.25,
+                               ifelse(Q20 == 3, 0.50,
+                                      ifelse(Q20 == 4, 0.75,
+                                             ifelse(Q20 == 5, 1, 0)))))) # Recoding the Likert scale from 0 to 1.
+
+mean(test$anger)
+
+  #Group B - White-on-Black
+B_wob <- dat %>% 
+  filter(wob == 1) %>% 
+  mutate(anger = ifelse(Q20 >3, 1, 0))
+
+t.test(A_control$anger)
+#Group C - White-on-White
+C_wow <- dat %>% 
+  filter(wow == 1)
+
+#Group D - Black-on-White
+D_bow <- dat %>% 
+  filter(bow == 1)
+
+#Group E - Black-on-Black
+E_bob <- dat %>% 
+  filter(bob == 1)
+
+
+## T-Test to set p-value to 0-10 or is it set when doing the table?
+## Jak udelam ten test
