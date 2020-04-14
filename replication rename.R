9ab1b73a37017bec48e6a361564d0fca4f022ceb
Gabriel Pisani <pisani.gabriel@seznam.cz>
2020-04-14 19:20:34 +0200

    test1 - loading data

--- replication rename.R
+++ replication rename.R
@@ -2,7 +2,11 @@
 # Load packages -----------------------------------------------------------
 
 library(tidyverse)
+library(here)
 
 # Load data ---------------------------------------------------------------
 
+dta <- haven::read_dta(here::here("Replication Data.dta"))
+
+# Data Wrangling
 
