c877c27413a85f8e1b92390b41c50cee74b999bf
Gabriel Pisani <pisani.gabriel@seznam.cz>
2020-04-17 02:41:57 +0200

    version 1

--- replication rename.R
+++ replication rename.R
@@ -3,6 +3,7 @@
 
 library(tidyverse)
 library(here)
+library(stargazer)
 
 # Load data ---------------------------------------------------------------
 
@@ -15,7 +16,10 @@ dat <- dat %>%
   mutate(Q20 = as.numeric(Q20)) %>%
   drop_na(Q20) %>% 
   mutate(Q43 = as.numeric(Q43)) %>% 
-  drop_na(Q43)
+  drop_na(Q43) %>% 
+  mutate(Q28 = as.numeric(Q28)) %>% 
+  drop_na(Q28)
+  
 #Creating groups for different experimental conditions
 
 #Group A - control group
@@ -112,8 +116,120 @@ E_bob <- dat %>%
                            ifelse(Q28 == 2, 0.75,
                                   ifelse(Q28 == 3, 0.50,
                                          ifelse(Q28 == 4, 0.25,
-                                                ifelse(Q28 == 5, 0, 0))))))
+                                                ifelse(Q28 == 5, 0, 0)))))) 
+  
+#Creating reduced objects via selecting needed dependent variables
+
+A <- summarize(A_control, anger_mean = mean(anger), shame_mean = mean(shame), punitive_mean = mean(punitive))
+
+B <- summarize(B_wob, anger_mean = mean(anger), shame_mean = mean(shame), punitive_mean = mean(punitive))
+
+C <- summarize(C_wow, anger_mean = mean(anger), shame_mean = mean(shame), punitive_mean = mean(punitive))
+
+D <- summarize(D_bow, anger_mean = mean(anger), shame_mean = mean(shame), punitive_mean = mean(punitive))
+
+E <- summarize(E_bob, anger_mean = mean(anger), shame_mean = mean(shame), punitive_mean = mean(punitive))
+
+# dat (tgroup2) > vytvoreni nove promenne angry, shame, punitive a jeji recode > vytvoreni pěti nových variables A B C D E > olabelovani techto novych variables
 
 
-## T-Test to set p-value to 0-10 or is it set when doing the table?
-## Jak udelam ten test
+install.packages("margins")
+library(margins)  
+
+test <- lm(A_control$anger ~ A_control)
+  
+
+anger_model_1 <- A$anger_mean - B$anger_mean
+
+anger_model_2 <- B$anger_mean - E$anger_mean
+
+anger_model_3 <- B$anger_mean - C$anger_mean
+
+
+shame_model_1 <- A$shame_mean - B$shame_mean
+
+shame_model_2 <- B$shame_mean - E$shame_mean
+
+shame_model_3 <- B$shame_mean - C$shame_mean
+
+
+punitive_model_1 <- A$punitive_mean - B$punitive_mean
+
+punitive_model_2 <- B$punitive_mean - E$punitive_mean
+
+punitive_model_3 <- C$punitive_mean - D$punitive_mean
+
+punitive_model_4 <- B$punitive_mean - C$punitive_mean
+
+
+# T-Test -------------------------------------------------------
+
+# We have to run 4 T-Tests. Each one comparing one group with the control one. We are looking for difference between the mean of the group adn the control group. 
+
+# ANGER
+
+#Control and B
+t.test(x = A_control$anger, y = B_wob$anger, mu = 0, alternative = "two.sided", conf.level = 0.90, var.equal = TRUE, paired = FALSE) # The p-value for the Table 1 in the article is set to 10 % so I set the confidence interval for 90 %.
+
+#Control and C
+t.test(x = A_control$anger, y = C_wow$anger, mu = 0, alternative = "two.sided", conf.level = 0.90, var.equal = TRUE, paired = FALSE)
+
+#Control and D
+t.test(x = A_control$anger, y = D_bow$anger, mu = 0, alternative = "two.sided", conf.level = 0.90, var.equal = TRUE, paired = FALSE)
+
+#Control and E
+t.test(x = A_control$anger, y = E_bob$anger, mu = 0, alternative = "two.sided", conf.level = 0.90, var.equal = TRUE, paired = FALSE)
+
+
+# SHAME
+
+#Control and B
+t.test(x = A_control$shame, y = B_wob$shame, mu = 0, alternative = "two.sided", conf.level = 0.90, var.equal = TRUE, paired = FALSE) # The p-value for the Table 1 in the article is set to 10 % so I set the confidence interval for 90 %.
+
+#Control and C
+t.test(x = A_control$shame, y = C_wow$shame, mu = 0, alternative = "two.sided", conf.level = 0.90, var.equal = TRUE, paired = FALSE)
+
+#Control and D
+t.test(x = A_control$shame, y = D_bow$shame, mu = 0, alternative = "two.sided", conf.level = 0.90, var.equal = TRUE, paired = FALSE)
+
+#Control and E
+t.test(x = A_control$shame, y = E_bob$shame, mu = 0, alternative = "two.sided", conf.level = 0.90, var.equal = TRUE, paired = FALSE)
+
+
+# PUNITIVE
+
+#Control and B
+t.test(x = A_control$punitive, y = B_wob$punitive, mu = 0, alternative = "two.sided", conf.level = 0.90, var.equal = TRUE, paired = FALSE)
+
+#Control and C
+t.test(x = A_control$punitive, y = C_wow$punitive, mu = 0, alternative = "two.sided", conf.level = 0.90, var.equal = TRUE, paired = FALSE)
+
+#Control and D
+t.test(x = A_control$punitive, y = D_bow$punitive, mu = 0, alternative = "two.sided", conf.level = 0.90, var.equal = TRUE, paired = FALSE)
+
+#Control and E
+t.test(x = A_control$punitive, y = E_bob$punitive, mu = 0, alternative = "two.sided", conf.level = 0.90, var.equal = TRUE, paired = FALSE)
+
+
+# Making tables -----------------------------------------------------------
+
+# Plotting ----------------------------------------------------------------
+
+# MARGINS PLOT
+
+figure_1 <- ggplot() +
+  geom_histogram(aes(dat_figure_1))
+
+figure_1
+
+
+####TO DO
+#### Stargazer type PŘEDĚLAT NA LATEX, TO UDELA V MARKDOWN DO PDF PEKNOU TABULKU.
+#### # Jak ale nastavit tu p-value, co se bude odvijet od control group?
+
+##PROBLEMS
+## number of individuals is different than in the original study (difference in one observation) in group three and four
+
+
+
+
